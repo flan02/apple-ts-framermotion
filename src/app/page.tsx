61e4a99860eacbd192f60b7dc4477f6901ee7910
flan02 <chanivetdan@hotmail.com>
2024-04-11 21:27:51 -0300

    Initial Nextjs v14.1.0 project.

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-rose-700 text-4xl">Hello Ollie!</h1>
+      <p className="text-yellow-400 font-bold">where your father is</p>
+    </main>
+  );
+}
